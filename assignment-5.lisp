ad1a9e5c3a9f7296d7e242244540a72ae7bdb0c0
Gordon Zheng <gordon@greenhouse.io>
2016-10-08 16:52:38 -0400

    Add search comparison code for assignment 5

--- assignment-5.lisp
+++ assignment-5.lisp
@@ -3,9 +3,9 @@
 
 ; here we go
 
-; (print (path (breadth-first-search *missionaries-and-cannibals*)))
+(print (path (breadth-first-search *missionaries-and-cannibals*)))
 ; (print (path (depth-first-search *missionaries-and-cannibals*)))
-(print (depth-first-search-with-duplicate-node-detection *missionaries-and-cannibals*))
+; (print (depth-first-search-with-duplicate-node-detection *missionaries-and-cannibals*))
 
 ; (defparameter *default-depth-limit* 12)
 ; (print (depth-first-search-with-depth-limit *missionaries-and-cannibals*))
@@ -13,4 +13,20 @@
 
 ; (print (steepest-ascent-hill-climbing *missionaries-and-cannibals*))
 ; (print (optimal-heuristic-search *missionaries-and-cannibals*))
-; (print (a-star-search *missionaries-and-cannibals*))
+; (print (path (a-star-search *missionaries-and-cannibals*)))
+
+(load "lib/farmer.lisp")
+
+; (print (breadth-first-search *farmer*))
+; (print (depth-first-search *farmer*))
+; (print (depth-first-search-with-duplicate-node-detection *farmer*))
+; (defparameter *default-depth-limit* 7)
+; (print (depth-first-search-with-depth-limit *farmer*))
+
+(load "lib/water-jug")
+
+; (print (breadth-first-search *water-jug*))
+; (print (depth-first-search *water-jug*))
+; (print (depth-first-search-with-duplicate-node-detection *water-jug*))
+; (defparameter *default-depth-limit* 6)
+; (print (depth-first-search-with-depth-limit *water-jug*))
