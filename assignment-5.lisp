14ae72d7fb706014886bdf788174e14734c4ee3a
Gordon Zheng <gordon@greenhouse.io>
2016-10-07 20:13:23 -0400

    Track boat position and add remaining operators

--- assignment-5.lisp
+++ assignment-5.lisp
@@ -10,7 +10,11 @@
   (c-left
     :initarg :c-left
     :accessor c-left
-    :documentation "cannibals on the left island")))
+    :documentation "cannibals on the left island")
+  (boat-is-left
+    :initarg :boat-is-left
+    :accessor boat-is-left
+    :documentation "true if boat is at left island")))
 
 (defmethod m-right ((self island-state))
   (- 3 (m-left self)))
@@ -37,79 +41,117 @@
 
 ; operators
 
-(defmethod all-over ((self island-state))
-  (let ((new (copy self)))
-    (setf (m-left new) 3)
-    (setf (c-left new) 3)
-    new))
-
-; operators:
-; move-2c-left
-; move-2c-right
-; move-1c-left
-; move-1c-right
-; move-2m-left
-; move-2m-right
-; move-1m-left
-; move-1m-right
-; move-1m1c-left
-; move-1m1c-right
-
-; MM CCCM
-; m: 1. c: 0
-
-(defmethod is-valid-state ((self island-state))
+(defmethod is-valid-successor ((previous island-state) (new island-state))
   (cond
-    ; check if action was not supposed to be performed
-    ((> (c-left self) 3) NIL)
-    ((> (m-left self) 3) NIL)
-    ((< (c-left self) 0) NIL)
-    ((< (m-left self) 0) NIL)
+    ; ensure that the direction of travel is valid:
+    ((equal (boat-is-left previous) (boat-is-left new)) NIL)
+    ; ensure enough cannibals or missionaries to perform action
+    ((> (c-left new) 3) NIL)
+    ((> (m-left new) 3) NIL)
+    ((< (c-left new) 0) NIL)
+    ((< (m-left new) 0) NIL)
     ; check if missionaries will be eaten:
     ((and
-      (> (c-left self) (m-left self))
-      (> (m-left self) 0)) NIL)
+      (> (c-left new) (m-left new))
+      (> (m-left new) 0)) NIL)
     ((and
-      (> (c-right self) (m-right self))
-      (> (m-right self) 0)) NIL)
+      (> (c-right new) (m-right new))
+      (> (m-right new) 0)) NIL)
     (T T)))
 
-(defmethod new-valid-state-or-nil ((self island-state) operator)
+(defmethod new-valid-successor-or-nil ((self island-state) operator)
   (let ((new (copy self)))
     (funcall operator new)
-    (when (is-valid-state new) new)))
+    (when (is-valid-successor self new) new)))
 
 (defmethod move-2c-left ((self island-state))
-    (new-valid-state-or-nil self
-      (lambda (new)
-        (setf (c-left new) (+ 2 (c-left new))))))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (c-left new) (+ 2 (c-left new)))
+      (setf (boat-is-left new) T))))
+
+(defmethod move-2c-right ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (c-left new) (+ -2 (c-left new)))
+      (setf (boat-is-left new) NIL))))
+
+(defmethod move-2m-left ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ 2 (m-left new)))
+      (setf (boat-is-left new) T))))
+
+(defmethod move-2m-right ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ -2 (m-left new)))
+      (setf (boat-is-left new) NIL))))
+
+(defmethod move-1c-left ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (c-left new) (+ 1 (c-left new)))
+      (setf (boat-is-left new) T))))
+
+(defmethod move-1c-right ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (c-left new) (+ -1 (c-left new)))
+      (setf (boat-is-left new) NIL))))
+
+(defmethod move-1m-left ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ 1 (m-left new)))
+      (setf (boat-is-left new) T))))
+
+(defmethod move-1m-right ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ -1 (m-left new)))
+      (setf (boat-is-left new) NIL))))
+
+(defmethod move-1m1c-left ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ 1 (m-left new)))
+      (setf (c-left new) (+ 1 (c-left new)))
+      (setf (boat-is-left new) T))))
+
+(defmethod move-1m1c-right ((self island-state))
+  (new-valid-successor-or-nil self
+    (lambda (new)
+      (setf (m-left new) (+ -1 (m-left new)))
+      (setf (c-left new) (+ -1 (c-left new)))
+      (setf (boat-is-left new) NIL))))
 
 (defparameter *missionaries-and-cannibals*
   (make-instance 'problem
-    :start-state (make-instance 'island-state :m-left 0 :c-left 0)
+    :start-state (make-instance 'island-state :m-left 0 :c-left 0 :boat-is-left NIL)
     :goal-test 'everyone-is-left
-    :operators '()
+    :operators '(move-2c-left move-2c-right move-1c-left move-1c-right move-2m-left move-2m-right move-1m-left move-1m-right move-1m1c-left move-1m1c-right)
     :name "missionaries and cannibals"))
 
 ; (describe (all-over (start-state *missionaries-and-cannibals*)))
 
-(describe (move-2c-left (make-instance 'island-state :m-left 0 :c-left 0)))
+(describe (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 0 :c-left 0)))
 
 ; TODO: write tests to ensure operators don't result in invalid states:
 
-(if (equal NIL (move-2c-left (make-instance 'island-state :m-left 0 :c-left 0)))
+(if (equal NIL (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 0 :c-left 0)))
   (print "fail") (print "pass"))
 
-(if (equal NIL (move-2c-left (make-instance 'island-state :m-left 0 :c-left 1)))
+(if (equal NIL (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 0 :c-left 1)))
   (print "fail") (print "pass"))
 
-(if (equal NIL (move-2c-left (make-instance 'island-state :m-left 0 :c-left 2)))
+(if (equal NIL (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 0 :c-left 2)))
   (print "pass") (print "fail"))
 
-(if (equal NIL (move-2c-left (make-instance 'island-state :m-left 1 :c-left 0)))
+(if (equal NIL (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 1 :c-left 0)))
   (print "pass") (print "fail"))
 
-(if (equal NIL (move-2c-left (make-instance 'island-state :m-left 2 :c-left 0)))
+(if (equal NIL (move-2c-left (make-instance 'island-state :boat-is-left NIL :m-left 2 :c-left 0)))
   (print "fail") (print "pass"))
 
-; (describe (BREADTH-FIRST-SEARCH *missionaries-and-cannibals*))
+(print (path (breadth-first-search *missionaries-and-cannibals*)))
