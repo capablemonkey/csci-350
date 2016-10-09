fe7d86440b7954d0495bf53d62b5b5b820230f25
Gordon Zheng <gordon@greenhouse.io>
2016-10-08 20:31:12 -0400

    Describe PQRS CSP

--- lib/csp.lisp
+++ lib/csp.lisp
@@ -13,17 +13,19 @@
 
 ; (print (all-diff 3 4 9 1 8 5 3))
 
-(setf variables '(tt w o ff u r c1 c2 c3))
+(setf variables '(p q r s tt w g h j c1 c2 c3 c4 c5))
 (setf domain '(0 1 2 3 4 5 6 7 8 9))
 (setf constraints
   (list
-    (list '(r) #'evenp)
-    (list '(tt w o ff u r) (lambda (&rest vars) (apply 'all-diff vars)))
-    (list '(o r c1) (lambda (o r c1) (equal (+ o o) (+ r (* c1 10)))))
-    (list '(w c1 u c2) (lambda (w c1 u c2) (equal (+ w w (* c1 10)) (+ u (* c2 10)))))
-    (list '(tt c2 o ff) (lambda (tt c2 o ff) (equal (+ tt tt (* c2 10)) (+ o (* ff 10)))))
-    (list '(ff) (lambda (ff) (> ff 0)))
-    (list '(ff c3) (lambda (ff c3) (equal ff c3)))
+    (list '(q) #'evenp)
+    (list '(w) (lambda (w) (> w 0)))
+    (list '(w c5) (lambda (ff c5) (equal ff c5)))
+    (list '(tt q c1) (lambda (tt q c1) (equal (+ tt tt) (+ q (* c1 10)))))
+    (list '(s c1 j c2) (lambda (s c1 j c2) (equal (+ s s c1) (+ j (* c2 10)))))
+    (list '(r c2 tt c3) (lambda (r c2 tt c3) (equal (+ r r c2) (+ tt (* c3 10)))))
+    (list '(q c3 h c4) (lambda (q c3 h c4) (equal (+ q q c3) (+ h (* c4 10)))))
+    (list '(p w c4 g c5) (lambda (p w c4 g c5) (equal (+ p w c4) (+ g (* c5 10)))))
+    (list '(p q r s tt w g h j) (lambda (&rest vars) (apply 'all-diff vars)))
     ))
 
 (defun verify (constraint assignments)
@@ -49,25 +51,33 @@
 ; (print (verify-constraints constraints assignments))
 
 (defun brute-force (domain constraints)
-  (loop for tt in domain do
-    (loop for w in domain do
-      (loop for o in domain do
-        (loop for ff in domain do
-          (loop for u in domain do
-            (loop for r in domain do
-              (let ((assignments (make-hash-table)))
-                (setf (gethash 'tt assignments) tt)
-                (setf (gethash 'w assignments) w)
-                (setf (gethash 'o assignments) o)
-                (setf (gethash 'ff assignments) ff)
-                (setf (gethash 'u assignments) u)
-                (setf (gethash 'r assignments) r)
-                (setf (gethash 'c1 assignments) (carry (+ o o) 10))
-                (setf (gethash 'c2 assignments) (carry (+ w w (gethash 'c1 assignments)) 10))
-                (setf (gethash 'c3 assignments) (carry (+ tt tt (gethash 'c2 assignments)) 10))
-                (if (verify-constraints constraints assignments)
-                  (return-from brute-force assignments)))
-      )))))))
+  (loop for p in domain do
+    (loop for q in domain do
+      (loop for r in domain do
+        (loop for s in domain do
+          (loop for tt in domain do
+            (loop for w in domain do
+              (loop for g in domain do
+                (loop for h in domain do
+                  (loop for j in domain do
+                    (let ((assignments (make-hash-table)))
+                      (setf (gethash 'p assignments) p)
+                      (setf (gethash 'q assignments) q)
+                      (setf (gethash 'r assignments) r)
+                      (setf (gethash 's assignments) s)
+                      (setf (gethash 'tt assignments) tt)
+                      (setf (gethash 'w assignments) w)
+                      (setf (gethash 'g assignments) g)
+                      (setf (gethash 'h assignments) h)
+                      (setf (gethash 'j assignments) j)
+                      (setf (gethash 'c1 assignments) (carry (+ tt tt) 10))
+                      (setf (gethash 'c2 assignments) (carry (+ s s (gethash 'c1 assignments)) 10))
+                      (setf (gethash 'c3 assignments) (carry (+ r r (gethash 'c2 assignments)) 10))
+                      (setf (gethash 'c4 assignments) (carry (+ q q (gethash 'c3 assignments)) 10))
+                      (setf (gethash 'c5 assignments) (carry (+ p w (gethash 'c4 assignments)) 10))
+                      (if (verify-constraints constraints assignments)
+                        (return-from brute-force assignments)))
+      ))))))))))
 
 (let ((results (brute-force domain constraints)))
   (if results
