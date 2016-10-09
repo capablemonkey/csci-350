008a8ed59c0d9fe32edb4fe129c25e92be0ed0e0
Gordon Zheng <gordon@greenhouse.io>
2016-10-08 20:11:50 -0400

    Solve TWO + TWO = FOUR with simple search

--- lib/csp.lisp
+++ lib/csp.lisp
@@ -1,3 +1,7 @@
+(defun carry (a b)
+  (multiple-value-bind (quot rem) (floor a b)
+    quot))
+
 (defun all-diff (&rest list)
   (let ((sorted-list (sort list #'<)))
     (loop
@@ -7,15 +11,20 @@
         (setf sorted-list (rest sorted-list)))
     T))
 
-(print (all-diff 3 4 9 1 8 5 3))
+; (print (all-diff 3 4 9 1 8 5 3))
 
-(setf variables '(a b c d))
+(setf variables '(tt w o ff u r c1 c2 c3))
 (setf domain '(0 1 2 3 4 5 6 7 8 9))
 (setf constraints
   (list
-    (list '(a b c d) (lambda (a b c d) (equal (+ a b) (+ c d))))
-    (list '(a b c d) (lambda (a b c d) (all-diff a b c d)))
-    (list '(a) #'oddp)))
+    (list '(r) #'evenp)
+    (list '(tt w o ff u r) (lambda (&rest vars) (apply 'all-diff vars)))
+    (list '(o r c1) (lambda (o r c1) (equal (+ o o) (+ r (* c1 10)))))
+    (list '(w c1 u c2) (lambda (w c1 u c2) (equal (+ w w (* c1 10)) (+ u (* c2 10)))))
+    (list '(tt c2 o ff) (lambda (tt c2 o ff) (equal (+ tt tt (* c2 10)) (+ o (* ff 10)))))
+    (list '(ff) (lambda (ff) (> ff 0)))
+    (list '(ff c3) (lambda (ff c3) (equal ff c3)))
+    ))
 
 (defun verify (constraint assignments)
   (let* (
@@ -23,7 +32,7 @@
     (predicate (second constraint))
     (values (mapcar (lambda (key) (gethash key assignments)) variables))
     (satisfied? (apply predicate values)))
-      (format t "~%* constraint ~a satisfied? [~a]" constraint satisfied?)
+      ; (format t "~%* constraint ~a satisfied? [~a]" constraint satisfied?)
       satisfied?))
 
 (defun verify-constraints (constraints assignments)
@@ -40,22 +49,33 @@
 ; (print (verify-constraints constraints assignments))
 
 (defun brute-force (domain constraints)
-  (loop for a in domain do
-    (loop for b in domain do
-      (let ((assignments (make-hash-table)))
-        (setf (gethash 'a assignments) a)
-        (setf (gethash 'b assignments) b)
-        (setf (gethash 'c assignments) 7)
-        (setf (gethash 'd assignments) 1)
-        (if (verify-constraints constraints assignments)
-          (return-from brute-force assignments)))
-      )))
+  (loop for tt in domain do
+    (loop for w in domain do
+      (loop for o in domain do
+        (loop for ff in domain do
+          (loop for u in domain do
+            (loop for r in domain do
+              (let ((assignments (make-hash-table)))
+                (setf (gethash 'tt assignments) tt)
+                (setf (gethash 'w assignments) w)
+                (setf (gethash 'o assignments) o)
+                (setf (gethash 'ff assignments) ff)
+                (setf (gethash 'u assignments) u)
+                (setf (gethash 'r assignments) r)
+                (setf (gethash 'c1 assignments) (carry (+ o o) 10))
+                (setf (gethash 'c2 assignments) (carry (+ w w (gethash 'c1 assignments)) 10))
+                (setf (gethash 'c3 assignments) (carry (+ tt tt (gethash 'c2 assignments)) 10))
+                (if (verify-constraints constraints assignments)
+                  (return-from brute-force assignments)))
+      )))))))
 
 (let ((results (brute-force domain constraints)))
   (if results
-    (format t "~%Solution found: a: ~a | b: ~a | c: ~a | d: ~a"
-      (gethash 'a results)
-      (gethash 'b results)
-      (gethash 'c results)
-      (gethash 'd results))
+    (format t "~%Solution found: tt: ~a | w: ~a | o: ~a | ff: ~a | u: ~a | r: ~a"
+      (gethash 'tt results)
+      (gethash 'w results)
+      (gethash 'o results)
+      (gethash 'ff results)
+      (gethash 'u results)
+      (gethash 'r results))
     (format t "~%No solution found")))
