23d7250519d69e873a6ba3b6fbbf7132f77a5c31
Gordon Zheng <gordon@greenhouse.io>
2016-10-15 17:17:04 -0400

    Re-organize code

--- lib/csp.lisp
+++ lib/csp.lisp
@@ -1,3 +1,7 @@
+;
+; Utility functions:
+;
+
 (defun carry (a b)
   (multiple-value-bind (quot rem) (floor a b)
     quot))
@@ -22,8 +26,35 @@
         b))
     a))
 
+(defun print-hash (hash)
+  (loop for k being the hash-keys in hash using (hash-value v)
+    do (format t "~a => ~a~%" k v)))
+
 (defun concat-string (list)
   (format nil "~{~a~}" list))
+
+(defun my-map-hash (func hash)
+  (loop for k being the hash-keys in hash using (hash-value v)
+    collect (funcall func k v)))
+
+(defun get-values (hash)
+  (my-map-hash (lambda (k v) v) hash))
+
+; snippet from http://stackoverflow.com/a/26061176:
+(defun copy-hash-table (hash-table)
+  (let ((ht (make-hash-table 
+             :test (hash-table-test hash-table)
+             :rehash-size (hash-table-rehash-size hash-table)
+             :rehash-threshold (hash-table-rehash-threshold hash-table)
+             :size (hash-table-size hash-table))))
+    (loop for key being each hash-key of hash-table
+       using (hash-value value)
+       do (setf (gethash key ht) value)
+       finally (return ht))))
+
+;
+; Class definitions:
+;
 
 (defclass constraint ()
   ((scope
@@ -52,130 +83,6 @@
     (mapcar
       (lambda (_) (default-domain self))
       (varyables self))))
-
-(defclass assignment ()
-  ((assigned-values
-    :initarg :assigned-values
-    :accessor assigned-values)
-  (updated-domains
-    :initarg :updated-domains
-    :accessor updated-domains)))
-
-(defun my-map-hash (func hash)
-  (loop for k being the hash-keys in hash using (hash-value v)
-    collect (funcall func k v)))
-
-(defun get-values (hash)
-  (my-map-hash (lambda (k v) v) hash))
-
-(defmethod is-complete ((self assignment))
-  (notany #'null (get-values (assigned-values self))))
-
-(defmethod is-consistent ((self assignment) csp)
-  (notany #'null
-    ; for each constraint:
-    ; return T if not all variables in scope are assigned
-    ; return T if all variables assigned and predicate passes with values
-    ; return F if all variables assigned but predicate fails with values
-    (mapcar
-      (lambda (constraint)
-        (let*
-          ((values-for-varyable-in-scope
-            (mapcar
-              (lambda (varyable) (gethash varyable (assigned-values self)))
-              (scope constraint)))
-          (all-variables-in-scope-assigned
-            (notany #'null values-for-varyable-in-scope)))
-
-          (if all-variables-in-scope-assigned
-            (apply (predicate constraint) values-for-varyable-in-scope)
-            T)))
-      (constraints csp))))
-
-(defmethod is-solution ((self assignment) csp)
-  (and
-    (is-complete self)
-    (is-consistent self csp)))
-
-(defmethod is-dead-end ((self assignment))
-  (some #'null
-    (mapcar
-      (lambda (varyable) (gethash varyable (updated-domains self)))
-      (unassigned-varyables self))))
-
-; snippet from http://stackoverflow.com/a/26061176:
-(defun copy-hash-table (hash-table)
-  (let ((ht (make-hash-table 
-             :test (hash-table-test hash-table)
-             :rehash-size (hash-table-rehash-size hash-table)
-             :rehash-threshold (hash-table-rehash-threshold hash-table)
-             :size (hash-table-size hash-table))))
-    (loop for key being each hash-key of hash-table
-       using (hash-value value)
-       do (setf (gethash key ht) value)
-       finally (return ht))))
-
-(defmethod copy ((self assignment))
-  (make-instance 'assignment
-    :assigned-values (copy-hash-table (assigned-values self))
-    :updated-domains (copy-hash-table (updated-domains self))))
-
-(defun print-hash (hash)
-  (loop for k being the hash-keys in hash using (hash-value v)
-    do (format t "~a => ~a~%" k v)))
-
-(defmethod print-assignment ((self assignment))
-  (format t "~%assigned values:~%")
-  (print-hash (assigned-values self))
-  (format t "~%updated domains:~%")
-  (print-hash (updated-domains self)))
-
-(defmethod unassigned-varyables ((self assignment))
-  (loop for k being the hash-keys in (assigned-values self) using (hash-value v)
-    when (null v)
-    collect k))
-
-(defmethod varyable-with-min-remaining-values ((self assignment))
-  (let*
-    ((vars-to-domain-lengths
-      (mapcar
-        (lambda (varyable) (list varyable (length (gethash varyable (updated-domains self)))))
-        (unassigned-varyables self)))
-    (sorted-vars-to-domain-lengths
-      (sort vars-to-domain-lengths
-      (lambda (tuple-a tuple-b)
-        (< (second tuple-a) (second tuple-b))))))
-    ; (print-hash (assigned-values self))
-    ; (print (unassigned-varyables self))
-    ; (print sorted-vars-to-domain-lengths)
-    (first (first sorted-vars-to-domain-lengths))))
-
-(defmethod index-of-varyable ((self csp) varyable)
-  (position varyable (varyables self)))
-
-(defmethod domain-for-varyable ((self csp) varyable)
-  (nth (index-of-varyable self varyable) (domains self)))
-
-(defmethod empty-assignment ((self CSP))
-  (let
-    ((assigned-values (make-hash-table))
-    (updated-domains (make-hash-table)))
-
-    (loop
-      for varyable in (varyables self)
-      for domain in (domains self) do
-        (setf (gethash varyable assigned-values) NIL)
-        (setf (gethash varyable updated-domains) domain))
-
-    (make-instance 'assignment
-      :assigned-values assigned-values
-      :updated-domains updated-domains)))
-
-(defmethod get-domain-for ((self assignment) varyable)
-  (gethash varyable (updated-domains self)))
-
-(defmethod set-domain-for ((self assignment) varyable new-domain)
-  (setf (gethash varyable (updated-domains self)) new-domain))
 
 ; ; TODO: account for bidirectional constraints.  all binary constraints currectly directional
 ; ; For now, we'll write bi-directional constraints as separate constraints for each way
@@ -251,23 +158,6 @@
         (equal 1 (position varyable-b (scope constraint)))))
     (binary-constraints-on self varyable-a)))
 
-(defmethod revise ((self assignment) csp varyable-a varyable-b)
-  (let ((changed NIL))
-    (loop for constraint in (binary-constraints-with csp varyable-a varyable-b) do
-      (let* (
-        (domain-a (get-domain-for self varyable-a))
-        (domain-b (get-domain-for self varyable-b))
-        (new-domain-a
-        (remove-if-not
-          (lambda (value)
-            (some (lambda (other-value) (funcall (predicate constraint) value other-value))
-              domain-b))
-          domain-a)))
-        (unless (equal (length domain-a) (length new-domain-a))
-          (setf changed T))
-        (set-domain-for self varyable-a new-domain-a)))
-    changed))
-
 (defmethod make-csp-node-consistent ((self CSP))
   (setf (domains self)
     (mapcar
@@ -286,6 +176,132 @@
               domain))))
       (varyables self))))
 
+;
+; Assignment (search state):
+;
+
+(defclass assignment ()
+  ((assigned-values
+    :initarg :assigned-values
+    :accessor assigned-values)
+  (updated-domains
+    :initarg :updated-domains
+    :accessor updated-domains)))
+
+(defmethod is-complete ((self assignment))
+  (notany #'null (get-values (assigned-values self))))
+
+(defmethod is-consistent ((self assignment) csp)
+  (notany #'null
+    ; for each constraint:
+    ; return T if not all variables in scope are assigned
+    ; return T if all variables assigned and predicate passes with values
+    ; return F if all variables assigned but predicate fails with values
+    (mapcar
+      (lambda (constraint)
+        (let*
+          ((values-for-varyable-in-scope
+            (mapcar
+              (lambda (varyable) (gethash varyable (assigned-values self)))
+              (scope constraint)))
+          (all-variables-in-scope-assigned
+            (notany #'null values-for-varyable-in-scope)))
+
+          (if all-variables-in-scope-assigned
+            (apply (predicate constraint) values-for-varyable-in-scope)
+            T)))
+      (constraints csp))))
+
+(defmethod is-solution ((self assignment) csp)
+  (and
+    (is-complete self)
+    (is-consistent self csp)))
+
+(defmethod is-dead-end ((self assignment))
+  (some #'null
+    (mapcar
+      (lambda (varyable) (gethash varyable (updated-domains self)))
+      (unassigned-varyables self))))
+
+(defmethod copy ((self assignment))
+  (make-instance 'assignment
+    :assigned-values (copy-hash-table (assigned-values self))
+    :updated-domains (copy-hash-table (updated-domains self))))
+
+(defmethod print-assignment ((self assignment))
+  (format t "~%assigned values:~%")
+  (print-hash (assigned-values self))
+  (format t "~%updated domains:~%")
+  (print-hash (updated-domains self)))
+
+(defmethod unassigned-varyables ((self assignment))
+  (loop for k being the hash-keys in (assigned-values self) using (hash-value v)
+    when (null v)
+    collect k))
+
+(defmethod varyable-with-min-remaining-values ((self assignment))
+  (let*
+    ((vars-to-domain-lengths
+      (mapcar
+        (lambda (varyable) (list varyable (length (gethash varyable (updated-domains self)))))
+        (unassigned-varyables self)))
+    (sorted-vars-to-domain-lengths
+      (sort vars-to-domain-lengths
+      (lambda (tuple-a tuple-b)
+        (< (second tuple-a) (second tuple-b))))))
+    ; (print-hash (assigned-values self))
+    ; (print (unassigned-varyables self))
+    ; (print sorted-vars-to-domain-lengths)
+    (first (first sorted-vars-to-domain-lengths))))
+
+(defmethod index-of-varyable ((self csp) varyable)
+  (position varyable (varyables self)))
+
+(defmethod domain-for-varyable ((self csp) varyable)
+  (nth (index-of-varyable self varyable) (domains self)))
+
+(defmethod get-domain-for ((self assignment) varyable)
+  (gethash varyable (updated-domains self)))
+
+(defmethod set-domain-for ((self assignment) varyable new-domain)
+  (setf (gethash varyable (updated-domains self)) new-domain))
+
+(defmethod empty-assignment ((self CSP))
+  (let
+    ((assigned-values (make-hash-table))
+    (updated-domains (make-hash-table)))
+
+    (loop
+      for varyable in (varyables self)
+      for domain in (domains self) do
+        (setf (gethash varyable assigned-values) NIL)
+        (setf (gethash varyable updated-domains) domain))
+
+    (make-instance 'assignment
+      :assigned-values assigned-values
+      :updated-domains updated-domains)))
+
+;
+; Arc consistency:
+;
+
+(defmethod revise ((self assignment) csp varyable-a varyable-b)
+  (let ((changed NIL))
+    (loop for constraint in (binary-constraints-with csp varyable-a varyable-b) do
+      (let* (
+        (domain-a (get-domain-for self varyable-a))
+        (domain-b (get-domain-for self varyable-b))
+        (new-domain-a
+        (remove-if-not
+          (lambda (value)
+            (some (lambda (other-value) (funcall (predicate constraint) value other-value))
+              domain-b))
+          domain-a)))
+        (unless (equal (length domain-a) (length new-domain-a))
+          (setf changed T))
+        (set-domain-for self varyable-a new-domain-a)))
+    changed))
+
 (defmethod make-arc-consistent ((self assignment) csp)
   ; arcs = scopes of binary constraints
   ; for each arc in arcs: revise ((get-varyable-by-name (first arc)) (get-varyable-by-name (second arc)))
@@ -296,6 +312,10 @@
         (if (revise self csp (first arc) (second arc))
           (loop for constraint-on-other in (binary-constraints-on csp (second arc)) do
             (push (scope constraint-on-other) queue)))))) ; TODO: limit to only where (second arc) is first in scope
+
+;
+; backtracking with maintain arc consistency heuristic:
+;
 
 (defun backtrack (assignment csp)
   (if (is-complete assignment)
@@ -308,7 +328,6 @@
         ; constrain the domain to the chosen value
         (setf (gethash unassigned-var (updated-domains new-assignment)) (list value))
         (make-arc-consistent new-assignment csp)
-        ; TODO: check if dead end (any domains are empty given the new assignment)
         (if (and (is-consistent new-assignment csp) (not (is-dead-end new-assignment)))
           (let ((result (backtrack new-assignment csp)))
             (if (not (null result))
@@ -321,6 +340,10 @@
     (make-arc-consistent assignment self)
     ; (print-assignment assignment)
     (backtrack assignment self)))
+
+;
+; Problem definition:
+;
 
 (defparameter *two-four*
   (make-instance 'CSP
